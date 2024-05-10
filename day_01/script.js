fd6bbc9861e3b26e7928d1d119c78185aa40e76e
divyanshu tailor <128120812+divyyuanshutailor@users.noreply.github.com>
2024-05-10 12:40:37 +0530

    inital commit 2024

---
+++ day_01/script.js
@@ -0,0 +1,23 @@
+// variable using
+// "use strict";
+const name = "john cena";
+var a = "namer"
+console.log(a)
+console.log("heloo")
+
+
+// datatype in js
+let nam = 5885;
+console.log(typeof nam)
+
+// convesion of data type
+
+result = "44646";
+console.log(result)
+console.log(typeof result)
+
+// explition of datatype
+divyanshu = -852.5546;
+divyanshu = parseInt(divyanshu);
+console.log(divyanshu)
+console.log(typeof divyanshu);
