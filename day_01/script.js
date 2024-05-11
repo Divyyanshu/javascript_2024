0b9261e1c08fd330d15769d89c740842bb601e24
divyanshu tailor <128120812+divyyuanshutailor@users.noreply.github.com>
2024-05-11 13:04:41 +0530

    secound commit 2024

--- day_01/script.js
+++ day_01/script.js
@@ -1,23 +1,30 @@
 // variable using
 // "use strict";
-const name = "john cena";
-var a = "namer"
-console.log(a)
-console.log("heloo")
+// const name = "john cena";
+// var a = "namer"
+// console.log(a)
+// console.log("heloo")
 
 
-// datatype in js
-let nam = 5885;
-console.log(typeof nam)
+// // datatype in js
+// let nam = 5885;
+// console.log(typeof nam)
 
-// convesion of data type
+// // convesion of data type
 
-result = "44646";
-console.log(result)
-console.log(typeof result)
+// result = "44646";
+// console.log(result)
+// console.log(typeof result)
 
-// explition of datatype
-divyanshu = -852.5546;
-divyanshu = parseInt(divyanshu);
-console.log(divyanshu)
-console.log(typeof divyanshu);
+// // explition of datatype
+// divyanshuji = -852.5546;
+// divyanshuji = parseInt(divyanshuji);
+// console.log(divyanshuji)
+// console.log(typeof divyanshuji);
+
+let  age = 30;
+if(age < 18 || age < 15){
+  console.log("not eleigbale")
+}else{
+console.log('eligilbe')
+}
